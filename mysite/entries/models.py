7403adf190affde52bf889a6eb07c0086e1a14d1
HuuXa <thxa.12b8.pdl@gmail.com>
2019-12-27 20:55:30 +0700

    update 27/12

--- mysite/entries/models.py
+++ mysite/entries/models.py
@@ -6,6 +6,7 @@ class Entry(models.Model):
     entry_text = models.TextField()
     entry_date = models.DateTimeField(auto_now_add=True)
     entry_author = models.ForeignKey(User, on_delete =  models.CASCADE)
+   
     
 
     
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password =password )
+            login(request, user)
+            return redirect('blog_home')
+            
+    else:
+        form = RegistrationForm()
+
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
